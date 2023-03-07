f0935dfc2e9f037f61e565969f1cf85ff67f9080
Matthew Randle <Matthew.Grayson.Randle@gmail.com>
2023-03-07 03:11:31 +0000

    Lab commit complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int test = 10;
+        test = test/0;
     }
 }
